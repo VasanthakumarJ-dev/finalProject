7c8e1bb2644e7a5212a6346992b60626c8d5888b
VasanthakumarJ-dev <j.vasanthakumar2018@gmail.com>
2024-10-17 12:40:08 +0530

    Delete backend directory

--- backend/models/VideoModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const videoSchema = new Schema({
-    video_url: { type: String, required: true },
-    video_title: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Video', videoSchema);
